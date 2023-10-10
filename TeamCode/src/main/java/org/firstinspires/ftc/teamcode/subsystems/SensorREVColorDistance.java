37505f2c444ecc98476102ae5d80c091e82f22cf
agopal <agopal@ash.nl>
2023-10-10 16:41:52 +0200

    Color sensing for spike marks: black and white

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SensorREVColorDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SensorREVColorDistance.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import android.app.Activity;
@@ -92,8 +63,17 @@ public class SensorREVColorDistance extends LinearOpMode {
             double percentageBlack = 1 - hsvValues[2];
 
             // print out the percentages
-            telemetry.addData("Percentage White", percentageWhite * 100);
-            telemetry.addData("Percentage Black", percentageBlack * 100);
+            double threshold = 0.7; // You can adjust this value based on your requirements
+
+            // Determine if the color is black or white based on the percentages and threshold
+            if (percentageWhite > 10000) {
+                telemetry.addData("Color", "White");
+            } else if (percentageWhite < 10000) {
+                telemetry.addData("Color", "Black");
+            } else {
+                telemetry.addData("Color", "Uncertain");
+            }
+
             telemetry.update();
 
             // change the background color to match the color detected by the RGB sensor.
