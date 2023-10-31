60c5cb695e25e6d47e8a6b18c2842d00d860dde5
agopal <agopal@ash.nl>
2023-10-31 16:24:58 +0100

    Intake moving motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.lib.systems.Subsystem;
+
+/**
+ * Just an example of how to move a motor with a subsystem.
+ */
+public class IntakeSubsystem extends Subsystem {
+    DcMotor motor;
+
+    @Override
+    public void init() {
+        motor = getHardwareMap().get(DcMotor.class, "motor");
+    }
+
+    @Override
+    public void periodic() {
+        motor.setPower(1);
+    }
+
+    @Override
+    public void onDisable() {
+        motor.setPower(0);
+    }
+}
