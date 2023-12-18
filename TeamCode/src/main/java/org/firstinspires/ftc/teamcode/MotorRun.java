1504fd8368c18f0b39726d80e9904928c1313f0c
whingss <123933300+whingssFTC@users.noreply.github.com>
2023-12-18 15:14:35 +0100

    add hang add correct positions for locking servo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorRun.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.lib.systems.Subsystems;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LinearSlideSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.MotorTestSubsystem;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name= "Motor Run", group="Testing")
+public class MotorRun extends OpMode {
+
+    private MotorTestSubsystem motorTestSubsystem;
+    //    private LinearSlideSubsystem linearSlideSubsystem;
+//
+//    private IntakeSubsystem intakeSubsystem;
+    @Override
+    public void init() {
+        motorTestSubsystem = new MotorTestSubsystem();
+        motorTestSubsystem.setGamepad(gamepad1);
+        Subsystems.onInit(this);
+
+//         linearSlideSubsystem = new LinearSlideSubsystem();
+//         linearSlideSubsystem.setGamepad(gamepad1);
+//         intakeSubsystem = new IntakeSubsystem();
+//         intakeSubsystem.setGamepad(gamepad1);
+
+        Subsystems.onInit(this);
+    }
+
+    @Override
+    public void loop() {
+        Subsystems.periodic();
+
+
+    }
+
+    @Override
+    public void stop() {
+        Subsystems.onDisable();
+    }
+}
